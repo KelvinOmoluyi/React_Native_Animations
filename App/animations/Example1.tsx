b5d87f4510b3ef3f797e1e2c2b11f5241ee66691
Kelvin Omoluyi <omoluyikelvin14@gmail.com>
2026-02-06 15:22:22 +0100

    Converted app.json to app.config.js, installed RN-skia, lottie, rebuilding native packages

--- App/animations/Example1.tsx
+++ App/animations/Example1.tsx
@@ -1,5 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Animated } from 'react-native'
+
+// Animated.Value
+// Animated.createAnimatedComponent
+// Animated.timing
+// Animated.spring
+// Animated.decay
+// Animated.parallel
+// Animated.sequence
+// Animated.stagger
+// Animated.loop
+// Animated.delay
+// Animated.event
 
 const Example1 = () => {
   return (
---
+++ app.config.js
@@ -0,0 +1,36 @@
+export default () => {
+  return {
+    name: "Animation_playground",
+    slug: "Animation_playground",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/images/icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/icon.png",
+        backgroundColor: "#ffffff",
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      package: "com.kelvinomoluyi.Animation_playground",
+    },
+    web: {
+      favicon: "./assets/images/icon.png",
+    },
+    extra: {
+      eas: {
+        projectId: "452a3346-38ab-4afd-9cbb-8b103fab5374",
+      },
+    },
+  };
+};
